f10a8087557c65d4c88fbe321a00428169e77d15
Raj-Gurjar <20ai1045@mitsgwl.ac.in>
2024-07-01 09:32:26 +0530

    f p2

--- src/components/Modal/VideoModal.js
+++ src/components/Modal/VideoModal.js
@@ -3,40 +3,27 @@ import { IoMdClose } from "react-icons/io";
 import "./VideoModal.scss";
 
 export default function VideoModal({ isOpen, closeModal, videoId }) {
-  useEffect(() => {
-    if (isOpen) {
-      document.body.style.overflowY = "hidden";
-    } else {
-      document.body.style.overflowY = "auto";
-    }
-    return () => {
-      document.body.style.overflowY = "auto";
-    };
-  }, [isOpen]);
-
   if (!isOpen) {
     return null;
   }
 
   return (
-    <div className="video-modal-overlay">
-      <div className="video-modal">
-        <div className="modal-content">
-          <button onClick={closeModal} className="close-btn">
-            <IoMdClose />
-          </button>
-          <div className="video-container">
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/IZW7iBscvvY?si=nyuE4S6f1zsbUGVq"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
-          </div>
+    <div className="video-modal">
+      <div className="modal-content">
+        <button onClick={closeModal} className="close-btn">
+          <IoMdClose />
+        </button>
+        <div className="video-container">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/IZW7iBscvvY?si=nyuE4S6f1zsbUGVq"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
         </div>
       </div>
     </div>
